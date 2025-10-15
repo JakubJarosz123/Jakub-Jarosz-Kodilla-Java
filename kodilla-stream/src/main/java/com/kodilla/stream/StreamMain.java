2d9814b2bc6f75c669b46fd3c5f27446c7fbdb39
Jakub Jarosz <jakubjarosz2015@gmail.com>
2025-10-15 15:26:04 +0200

    7.2 niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterable.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -11,5 +12,8 @@ public class StreamMain {
         poemBeautifier.beautify("Java programming", text -> "ABC " + text + " ABC");
         poemBeautifier.beautify("Java programming", text -> text.toUpperCase());
         poemBeautifier.beautify("Java programming", text -> text.toLowerCase());
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
