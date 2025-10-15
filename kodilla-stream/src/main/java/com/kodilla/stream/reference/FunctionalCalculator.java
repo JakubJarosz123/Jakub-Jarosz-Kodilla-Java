7467743731bd8673371456e0de54e2c35c05d1ac
Jakub Jarosz <jakubjarosz2015@gmail.com>
2025-10-15 13:29:18 +0200

    Moduł 7 Wstęp do programowania funkcyjnego

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/reference/FunctionalCalculator.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.reference;
+
+public class FunctionalCalculator {
+
+    public static double sumAToB (double a, double b) {
+        return a + b;
+    }
+
+    public static double subAFromB (double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplyAFromB (double a, double b) {
+        return a * b;
+    }
+
+    public static double divideAByB (double a, double b) {
+        return a / b;
+    }
+}
