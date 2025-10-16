bb62cfb5e550362d714aec92fcc75f6dc1c74372
Jakub Jarosz <jakubjarosz2015@gmail.com>
2025-10-16 15:50:07 +0200

    7.4 poznanie flatMap() i reduce()

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.invoice.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleInvoiceTestSuite {
+
+    @Test
+    void testGetValueToPay() {
+        //Given
+        SimpleInvoice invoice = new SimpleInvoice();
+        //When
+        invoice.addItem(new SimpleItem(new SimpleProduct("Prod1", 17.28), 2.0));
+        invoice.addItem(new SimpleItem(new SimpleProduct("Prod2", 11.99), 3.5));
+        invoice.addItem(new SimpleItem(new SimpleProduct("Prod3", 6.49), 5.0));
+        //Then
+        assertEquals(108.975,  invoice.getValueToPay(), 0.001);
+    }
+}
