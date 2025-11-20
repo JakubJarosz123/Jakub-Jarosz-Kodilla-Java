785e344c4e8caa5109d29a0ddfb18178a4e216aa
Jakub Jarosz <jakubjarosz2015@gmail.com>
2025-11-20 14:03:32 +0100

    kalkulator z wyswietlaczem

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void testCalculations() {
+        //Given
+        //When
+        calculator.add(5,5);
+        calculator.sub(5,5);
+        calculator.mul(5,5);
+        calculator.div(5,5);
+        //Then
+    }
+}
